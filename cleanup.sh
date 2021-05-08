6283965d4d0c66c0e00b95934ccdbccb09a6f164
Rainer Datzer <rdatz@gmx.de>
2021-05-08 04:27:38 +0200

    working on make

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf ".deps" "aclocal.m4" "autom4te.cache" "compile" "config.log" "config.status" "configure" "depcomp" "install-sh" "Makefile" "Makefile.in" "missing" "config.guess" "config.sub" ".libs"
+
